ef17c798ff4a573d96c174bbe89142596d8c584c
Silas Henrique <105889529+Kiamisa@users.noreply.github.com>
2025-08-27 09:46:09 -0300

    Add command handling for todo application
    Still not complete, implementation after work

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,19 @@ fn main(){
 
     let args: Vec<String> = env::args().collect();
 
-    
+    if args.len() > 1 {
+
+        let command = &args[1];
+
+        match &command[..] {
+            "list" => todo.list(),
+            "mk" => todo.add(&args[2..]),
+            "rm" => todo.remove(&args[2..]),
+            "done" => todo.done(&args[2..]),
+            "raw" => todo.raw(&args[2..]),            
+        }
+
+    } else {
+        todo.list();
+    }
 }
