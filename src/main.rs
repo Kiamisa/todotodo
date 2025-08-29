5fd3d6344fe239dbf3d6a6d834a4104bdb2fb2bf
Silas Henrique <105889529+Kiamisa@users.noreply.github.com>
2025-08-29 10:20:38 -0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::env;
-use todo_bin::{help, Todo};
+use todo_todo::{help, Todo};
 
 fn main(){
     let todo = Todo::new().expect("Não foi possível criar o TODO :(");
@@ -7,17 +7,19 @@ fn main(){
     let args: Vec<String> = env::args().collect();
 
     if args.len() > 1 {
-
         let command = &args[1];
-
         match &command[..] {
             "list" => todo.list(),
             "mk" => todo.add(&args[2..]),
             "rm" => todo.remove(&args[2..]),
             "done" => todo.done(&args[2..]),
-            "raw" => todo.raw(&args[2..]),            
+            "raw" => todo.raw(&args[2..]),
+            "edit" => todo.edit(&args[2..]),
+            "sort" => todo.sort(),
+            "reset" => todo.reset(),
+            "restore" => todo.restore(),
+            "help" | "--help" | "-h" | _ => help(),            
         }
-
     } else {
         todo.list();
     }
