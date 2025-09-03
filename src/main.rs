0d64c03e8aff647ffbd6de21a5dfc505b1de2c2d
Silas Henrique <105889529+Kiamisa@users.noreply.github.com>
2025-09-03 12:42:04 -0300

    Implement GUI option in main function

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,13 @@ mod cli;
 mod gui;
 mod libs;
 
-fn main(){
+fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() == 1 || args.contains(&"--gui".to_string()){
-        println!("GUI ainda não implementada, usando CLI");
-        cli::run(args);
+
+    if args.len() == 1 || args.contains(&"--gui".to_string()) {
+        let todo = libs::Todo::new().expect("Não foi possível criar o TODO :(");
+        gui::app::run(todo);
     } else {
         cli::run(args);
     }
-
 }
