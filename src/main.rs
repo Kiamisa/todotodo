2fb2ff293303af640fe5b4c4ce66c854752c93b8
Silas Henrique <105889529+Kiamisa@users.noreply.github.com>
2025-09-03 10:19:33 -0300

    Refactor main function to use CLI module

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,15 @@
 use std::env;
-use todo_todo::{help, Todo};
+mod cli;
+mod gui;
+mod libs;
 
 fn main(){
-    let todo = Todo::new().expect("Não foi possível criar o TODO :(");
-
     let args: Vec<String> = env::args().collect();
-
-    if args.len() > 1 {
-        let command = &args[1];
-        match &command[..] {
-            "list" => todo.list(),
-            "mk" => todo.add(&args[2..]),
-            "rm" => todo.remove(&args[2..]),
-            "done" => todo.done(&args[2..]),
-            "raw" => todo.raw(&args[2..]),
-            "edit" => todo.edit(&args[2..]),
-            "sort" => todo.sort(),
-            "reset" => todo.reset(),
-            "restore" => todo.restore(),
-            "help" | "--help" | "-h" | _ => help(),            
-        }
+    if args.len() == 1 || args.contains(&"--gui".to_string()){
+        println!("GUI ainda não implementada, usando CLI");
+        cli::run(args);
     } else {
-        todo.list();
+        cli::run(args);
     }
+
 }
